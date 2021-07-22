de2b01a6007fbb7de4ae660baaf4c6da724a45da
Alekhya1311 <alekhyabairaboina1311@gmail.com>
2021-07-22 20:10:01 +0530

    first commit

---
+++ src/components/Checkbox/index.js
@@ -0,0 +1,11 @@
+
+// const CheckBox = props => {
+//   const {id,handleCheckChieldElement,isChecked,value} = props
+//     return (
+//       <li>
+//        <input key={id} onClick={handleCheckChieldElement} type="checkbox" checked={isChecked} value={props.value} /> {props.value}
+//       </li>
+//     )
+// }
+
+// export default CheckBox
